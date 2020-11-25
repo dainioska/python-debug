2091c7df47fcf541a6b2cf7dc3070836722cd654
dainioshka <dainioshka@gmail.com>
2020-11-25 18:44:09 +0200

    commit>vsc_01

--- python_debug.py
+++ python_debug.py
@@ -4,8 +4,8 @@ values = [1, 2, 3, 4, 5]
 
 def element_on(numbers):
     total = 0
-    for index,number in enumerate(numbers):
-        pass
+    for index, number in enumerate(numbers):
+        total += number * (index - 1)
     return total
 
 print(element_on(values))

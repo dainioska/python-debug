d981c9ee24560f07c3c94cdbbe9ab6271d9333a6
dainioska <dainioska@yahoo.com>
2020-11-25 18:09:32 +0200

    commit>03

---
+++ python_debug.py
@@ -0,0 +1,13 @@
+#import random
+
+values = [1, 2, 3, 4, 5]
+
+def element_on(numbers):
+    total = 0
+    for index,number in enumerate(numbers):
+        pass
+    return total
+
+print(element_on(values))
+
+#print(random.randint)

4be659ce338c90089ce0753913fb689cdb1be4a4
dainioshka <dainioshka@gmail.com>
2020-12-16 16:57:19 +0200

    antras comm

--- mano-git.py
+++ mano-git.py
@@ -3,8 +3,16 @@
 class Git:
     def __init__(self,name, age):
         self.name = name
-        self.age =age
+        self.age = age
 
-    def read():
-        pass
+    def read(self,name):
+        print(name)
 
+    def __str__(self):
+        return self.name +', '+ str(self.age)
+
+
+
+        
+a = Git("alio",22)
+print(a)
